7dbd9c60a12565ad3a463da05892d4271f83e414
rcapillas <raulcapillas@gmail.com>
2023-04-26 20:46:12 +0200

    Modulo 3 COMPLETE

---
+++ Modulo3/src/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <img src={`./assets/logo.png`} alt="LemonCode"></img>
+      <h1>¡Hola mundo en React!</h1>
+    </>
+  );
+};
---
+++ Modulo3/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+  },
+};
