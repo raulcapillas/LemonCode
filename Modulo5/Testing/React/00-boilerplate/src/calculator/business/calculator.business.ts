34cdf5ab03faf6d3f4bf7bffed0378667a7b8907
rcapillas <raulcapillas@gmail.com>
2024-02-08 10:30:55 +0100

    testing exercice. hello and edit-name

--- Modulo5/Testing/React/00-boilerplate/src/calculator/business/calculator.business.ts
+++ Modulo5/Testing/React/00-boilerplate/src/calculator/business/calculator.business.ts
@@ -1,6 +1,5 @@
 export const isLowerThan = (value: number, max: number) => {
-    console.log(`The value: ${value} is lower than ${max}`);
-    
-}
+  console.log(`The value: ${value} is lower than ${max}`);
+};
 
 export const max = 6;
