3ca5576a50abea22d0fd45921e01d89f23738f0f
rcapillas <raulcapillas@gmail.com>
2024-02-13 11:08:35 +0100

    - Added moduleDirectories in jest config file - confirmation-dialog.hook.spec cases

--- Modulo5/real-project/src/common/components/confirmation-dialog/confirmation-dialog.hook.spec.ts
+++ Modulo5/real-project/src/common/components/confirmation-dialog/confirmation-dialog.hook.spec.ts
@@ -1,10 +1,54 @@
 import { renderHook } from '@testing-library/react';
 import { useConfirmationDialog } from './confirmation-dialog.hook';
+import { Lookup, createEmptyLookup } from 'common/models';
 
 describe('useConfirmationDialog specs', () => {
-  it('', () => {
+  const item: Lookup = {
+    id: '1',
+    name: 'John',
+  };
+
+  it('Should return an object with isOpen to be falsy, itemToDelete as createEmptyLookup and onAccept, onClose and onOpenDialog as function', () => {
     //  Arrage
     //  Act
+    const { result } = renderHook(() => useConfirmationDialog());
     //  Assert
+
+    expect(result.current.isOpen).toBeFalsy();
+    expect(result.current.itemToDelete).toEqual(createEmptyLookup());
+    expect(result.current.onAccept).toEqual(expect.any(Function));
+    expect(result.current.onClose).toEqual(expect.any(Function));
+    expect(result.current.onOpenDialog).toEqual(expect.any(Function));
+  });
+
+  it('Should return and object with isOpen true and itemToDelete as item', () => {
+    //  Arrage
+    //  Act
+    const renderUseConfirmationDialog = renderHook(() =>
+      useConfirmationDialog()
+    );
+    const { result } = renderUseConfirmationDialog;
+    result.current.onOpenDialog(item);
+    renderUseConfirmationDialog.rerender();
+
+    //  Assert
+    expect(result.current.isOpen).toBeTruthy();
+    expect(result.current.itemToDelete).toEqual(item);
+  });
+
+  it('Should return isOpen false  after calling onOpenDialog', () => {
+    //  Arrage
+    //  Act
+    const renderUseConfirmationDialog = renderHook(() =>
+      useConfirmationDialog()
+    );
+    const { result } = renderUseConfirmationDialog;
+    result.current.onOpenDialog(item);
+    result.current.onClose();
+    renderUseConfirmationDialog.rerender();
+
+    //  Assert
+    expect(result.current.isOpen).toBeFalsy();
+    expect(result.current.itemToDelete).toEqual(item);
   });
 });
