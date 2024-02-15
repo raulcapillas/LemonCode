3cc262ecd9f4b2f2fc8f26fcb25e9d350b3bccc6
rcapillas <raulcapillas@gmail.com>
2024-02-15 10:56:18 +0100

    Exercice testing confirmation-dialog.component.spec

--- Modulo5/real-project/src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
+++ Modulo5/real-project/src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 import { DialogProps } from './confirmation-dialog.interface';
 
 describe('confirmation dialog component specs', () => {
-  it('should not display a dialog', () => {
+  it('should not display a dialog when isOpen is false', () => {
     //  Arrage
     const dialogProps: DialogProps = {
       isOpen: false,
@@ -27,32 +28,85 @@ describe('confirmation dialog component specs', () => {
     //  Assert
     expect(container.firstChild).toBeNull();
   });
-  it('should display a dialog', async() => {
+
+  it('should display a dialog with title, content and labels accept and close buttons when isOpen is true', () => {
     //  Arrage
     const dialogProps: DialogProps = {
       isOpen: true,
       onAccept: () => {},
       onClose: () => {},
-      title: '',
+      title: 'Dialog test',
       labels: {
-        acceptButton: '',
-        closeButton: '',
+        acceptButton: 'Accept',
+        closeButton: 'Close',
       },
     };
 
     //  Act
-    const { container } = await render(
+    render(
       <ConfirmationDialogComponent dialogProps={dialogProps}>
-        <h1>test</h1>
+        <h1>Content test</h1>
       </ConfirmationDialogComponent>
     );
-    
+
     //  Assert
-    //expect(container.firstChild).not.toBeNull();
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Content test'
+    );
+    expect(screen.getByText('Dialog test')).toBeInTheDocument();
+    expect(screen.getByText('Accept')).toBeInTheDocument();
+    expect(screen.getByText('Close')).toBeInTheDocument();
   });
-  it('', () => {
+  it('should call onClose function when close button is clicked', async () => {
     //  Arrage
+    const dialogProps: DialogProps = {
+      isOpen: true,
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+      title: 'Dialog test',
+      labels: {
+        acceptButton: 'Accept',
+        closeButton: 'Close',
+      },
+    };
     //  Act
+    render(
+      <ConfirmationDialogComponent dialogProps={dialogProps}>
+        <h1>Content test</h1>
+      </ConfirmationDialogComponent>
+    );
+
+    const buttonElement = screen.getByRole('button', { name: /Close/i });
+
+    await userEvent.click(buttonElement);
+
     //  Assert
+    expect(dialogProps.onClose).toHaveBeenCalledTimes(1);
+  });
+  it('should call onAccept function when accept button is clicked', async () => {
+    //  Arrage
+    const dialogProps: DialogProps = {
+      isOpen: true,
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+      title: 'Dialog test',
+      labels: {
+        acceptButton: 'Accept',
+        closeButton: 'Close',
+      },
+    };
+    //  Act
+    render(
+      <ConfirmationDialogComponent dialogProps={dialogProps}>
+        <h1>Content test</h1>
+      </ConfirmationDialogComponent>
+    );
+
+    const buttonElement = screen.getByRole('button', { name: /Accept/i });
+
+    await userEvent.click(buttonElement);
+
+    //  Assert
+    expect(dialogProps.onAccept).toHaveBeenCalledTimes(1);
   });
 });
