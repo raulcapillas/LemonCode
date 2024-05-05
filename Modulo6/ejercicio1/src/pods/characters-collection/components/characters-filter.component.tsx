d67b066673875e7cebe699fd52bf6db42fdbc59f
Raúl <raulcapillas@gmail.com>
2024-05-05 15:46:26 +0200

    Delete SearchBar dependency (problems with MUI v4)

--- Modulo6/ejercicio1/src/pods/characters-collection/components/characters-filter.component.tsx
+++ Modulo6/ejercicio1/src/pods/characters-collection/components/characters-filter.component.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from "react";
-import { Paper } from "@mui/material";
-import SearchBar from "material-ui-search-bar";
+import {
+  Paper,
+  InputBase,
+  IconButton,
+  Box,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
 import { useCharacter } from "hooks/character-collection/character-collection.context";
 import { useCharactersCollection } from "../character-collection.hook";
+import ClearIcon from "@mui/icons-material/Clear";
+import SearchIcon from "@mui/icons-material/Search";
 
 interface Props {
   name: string;
@@ -27,12 +35,39 @@ export const CharactersFilterContainer: FC<Props> = ({ name, setName }) => {
 
   return (
     <Paper>
-      <SearchBar
-        value={name}
-        onChange={(searchName) => requestSearch(searchName)}
-        onCancelSearch={() => cancelSearch()}
-        placeholder="Search by name..."
-      />
+      <Box
+        sx={{
+          maxWidth: "100%",
+        }}
+      >
+        <TextField
+          fullWidth
+          label="Filter"
+          id="filter"
+          placeholder="Search by name..."
+          onChange={(event) => requestSearch(event.target.value)}
+          value={name}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  type="button"
+                  sx={{ p: "10px" }}
+                  aria-label="Clear"
+                  onClick={cancelSearch}
+                >
+                  <ClearIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
     </Paper>
   );
 };
